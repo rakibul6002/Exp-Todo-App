2c9874443ad6b0d108283833d440ec348d47073a
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-16 23:06:44 +0600

    go to home

--- src/app/AllTodo/page.js
+++ src/app/AllTodo/page.js
@@ -1,15 +1,23 @@
 'use client'
 import AllTodos from '@/components/AllTodos';
+import Link from 'next/link';
 import React from 'react';
 
 const AllTodopage = () => {
   return (
     <div>
       <div className='max-w-xl mx-auto border border-gray-400 mt-5 py-6 px-4'>
-        <p className='text-lg font-semibold '>All todos</p> 
+        <p className='text-lg font-semibold flex items-center justify-center'>All todos</p> 
         <div>
           <AllTodos/>
         </div>
+      </div>
+      <div className=  'flex items-center justify-center mx-auto mt-10'>
+        <Link href={'/'}>
+          <button className='bg-green-500 px-3 py-2 rounded-lg font-semibold'>
+            Go to Home page
+          </button>
+        </Link>
       </div>
     </div>
   )
